1d9af58e73c26e2965da81586ab906dce11a508c
Cedric Sirianni <cds1@sirianni.org>
2022-11-05 16:31:19 -0400

    add non-functional map use

--- maps/src/App.tsx
+++ maps/src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Map from "react-map-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { mapboxToken } from './private/variables';
 import './App.css';
 
+type ProvidenceLatLong = {
+  lat: number,
+  long: number
+}
+
 function App() {
+
+  const coordinates: ProvidenceLatLong = {
+    lat: 41.8240,
+    long: 71.4128
+  }
+
+  const [viewState, setViewState] = React.useState({
+    latitude: coordinates.lat,
+    longitude: coordinates.long,
+    zoom: 5
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Map mapboxAccessToken={mapboxToken}
+        longitude={viewState.longitude}
+        latitude={viewState.latitude}
+        zoom={viewState.zoom} />
     </div>
   );
 }
